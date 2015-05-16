ae870479ce7aabf55e6008cbf4af2415d6f1231d
hpinsley <hpinsley@gmail.com>
2015-05-16 15:11:28 -0400

    The app component can call the employee-list component.  Had to clone the typescript compiler, build it locally, and use it because the beta release has a bug with decorators.

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,4 @@
-if (typeof __decorate !== "function") __decorate = function (decorators, target, key, desc) {
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     if (typeof Reflect === "object" && typeof Reflect.decorate === "function") return Reflect.decorate(decorators, target, key, desc);
     switch (arguments.length) {
         case 2: return decorators.reduceRight(function(o, d) { return (d && d(o)) || o; }, target);
@@ -6,29 +6,30 @@ if (typeof __decorate !== "function") __decorate = function (decorators, target,
         case 4: return decorators.reduceRight(function(o, d) { return (d && d(target, key, o)) || o; }, desc);
     }
 };
-if (typeof __metadata !== "function") __metadata = function (k, v) {
+var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
-/// <reference path="../typings/angular2/angular2.d.ts" />
-/// <reference path="./employees.ts" />
-var angular2_1 = require('angular2/angular2');
-var employees_1 = require('./employees');
+import { Component, View, bootstrap } from 'angular2/angular2';
+import { EmployeeList } from './employees';
 var age = 24;
-var MyAppComponent = (function () {
-    function MyAppComponent() {
+let MyAppComponent = class {
+    constructor() {
         console.log("MyAppComponent has been constructed");
         this.name = 'Howard';
     }
-    MyAppComponent = __decorate([
-        angular2_1.Component({
-            selector: 'my-app',
-            directives: [employees_1.EmployeeList]
-        }),
-        angular2_1.View({
-            template: "<h1>Hello there {{ name }}  you are " + age + " years old</h1>\n    <h2>This is an H2</h2>\n    <employee-list></employee-list>\n    <div>After the employee list</div>\n    "
-        }), 
-        __metadata('design:paramtypes', [])
-    ], MyAppComponent);
-    return MyAppComponent;
-})();
-angular2_1.bootstrap(MyAppComponent);
+};
+MyAppComponent = __decorate([
+    Component({
+        selector: 'my-app'
+    }),
+    View({
+        template: `<h1>Hello there {{ name }}  you are ${age} years old</h1>
+    <h2>This is an H2</h2>
+    <employee-list></employee-list>
+    <div>After the employee list</div>
+    `,
+        directives: [EmployeeList]
+    }), 
+    __metadata('design:paramtypes', [])
+], MyAppComponent);
+bootstrap(MyAppComponent);
